99a4e301580c3f0bd58de282bf247165acb0bc91
mughees605 <mughees605@gmail.com>
2017-12-02 13:13:40 -0800

    initialize server

---
+++ Back-End/Timestamp-Microservice/index.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var port = process.env.PORT || 8080
+app.use(bodyparser.json());
+app.use(cors());
+
+
+
+app.listen(port,function(){
+    console.log(`working on localhost ${port}`)
+})
