5e5cdf57bdd99005540a0a5e1ad0b8f33a0978e4
mughees605 <mughees605@gmail.com>
2017-12-02 13:38:05 -0800

    completed TImestamp microservice

--- Back-End/Timestamp-Microservice/index.js
+++ Back-End/Timestamp-Microservice/index.js
@@ -5,7 +5,26 @@ var port = process.env.PORT || 8080
 app.use(bodyparser.json());
 app.use(cors());
 
+app.get('/timestamp/:date',function(req,res,next){
+    var natural , unix;
+    var value = req.params.date;
 
+    var formattedDate = {
+        year: 'numeric',
+        month: 'long',
+        day : 'numeric'
+    }
+     
+    if(isNaN(value)){
+        natural = new Date(value).toLocaleDateString('en-us',formattedDate);
+        unix = new Date(value).getTime()/1000 
+    }
+    else{
+        unix = value;
+        natural = new Date(value * 1000).toLocaleDateString('en-us',formattedDate)
+    }
+    res.send({ unix:unix , natural, natural});
+})
 
 app.listen(port,function(){
     console.log(`working on localhost ${port}`)
