b592608b2f9ecd897c6f53862bac87577e49a872
mughees605 <mughees605@gmail.com>
2017-12-03 15:17:22 +0500

    completed 2nd project

--- Back-End/Request-url-headers/index.js
+++ Back-End/Request-url-headers/index.js
@@ -5,10 +5,23 @@ var port = process.env.PORT || 8080
 app.use(bodyparser.json());
 app.use(cors());
 
-app.get('/api/whoai',function(req,res,next){
-   
+app.get('/api/whoai', function (req, res, next) {
+  
+
+    // https://stackoverflow.com/questions/8107856/how-to-determine-a-users-ip-address-in-node
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    let obj = {
+        ip: ip,
+        language: req.headers['accept-language'].split(',')[0],
+        OS: req.headers['user-agent'].split(') ')[0].split(' (')[1],
+    }
+    res.send(obj);
 })
 
-app.listen(port,function(){
+app.listen(port, function () {
     console.log(`working on localhost ${port}`)
 })
