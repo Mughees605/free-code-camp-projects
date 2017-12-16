bbe2fdd75ca806bb4968ddd7c999ee6079837485
mughees605 <mughees605@gmail.com>
2017-12-16 22:27:56 +0500

    get all poll onto UI

--- Back-End/voting-app/src/app/home/home.component.ts
+++ Back-End/voting-app/src/app/home/home.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PollService } from '../services/poll.service';
-
+import { CreatePoll } from '../models/newpoll.model';
+import { ISubscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
+  allPolls : CreatePoll[] 
   constructor(private pollSer:PollService) { }
 
   ngOnInit() {
-    this.pollSer.getAll().subscribe((res)=>{
-      console.log(res);
+    this.pollSer.getAll().subscribe((poll : CreatePoll[])=>{
+      this.allPolls = poll;
     })
   }
 
